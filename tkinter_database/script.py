3da4a347b2deb445dd964b65b5b43ceff1ad2a37
jingwei269 <jingwei269@gmail.com>
2021-10-08 16:52:09 +0800

    commit

---
+++ tkinter_database/script.py
@@ -0,0 +1,66 @@
+from tkinter import * 
+
+window=Tk()
+
+
+def view():
+    print("View ALL")
+
+l1 = Label(window,text="Title")
+l1.grid(row=0,column=0)
+
+e1_value=StringVar()
+e1=Entry(window,textvariable=e1_value)
+e1.grid(row=0,column=1) 
+
+l2 = Label(window,text="Author")
+l2.grid(row=0,column=2)
+
+e2_value=StringVar()
+e2=Entry(window,textvariable=e2_value)
+e2.grid(row=0,column=3) 
+
+l3 = Label(window,text="Year")
+l3.grid(row=1,column=0)
+
+e3_value=StringVar()
+e3=Entry(window,textvariable=e3_value)
+e3.grid(row=1,column=1) 
+
+l4 = Label(window,text="ISBN")
+l4.grid(row=1,column=2)
+
+e4_value=StringVar()
+e4=Entry(window,textvariable=e4_value)
+e4.grid(row=1,column=3) 
+books = ['1','2','3','1','2','3','1','2','3','1','2','3','1','2','3','1','2','3','1','2','3','1','2','3','1','2','3' ]
+list1=Listbox(window,height=12,width=46)
+list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+for book in books:
+    list1.insert("end",book)
+sb1=Scrollbar(window)
+sb1.grid(row=2,column=2,rowspan=6)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+b1 = Button(window, text="View all",width=9,command=view)
+b1.grid(row=2,column=3)
+
+b2 = Button(window, text="Search entry",width=9,command=view)
+b2.grid(row=3,column=3)
+
+b3 = Button(window, text="Add entry",width=9,command=view)
+b3.grid(row=4,column=3)
+
+b4 = Button(window, text="Update",width=9,command=view)
+b4.grid(row=5,column=3)
+
+b5 = Button(window, text="Delete",width=9,command=view)
+b5.grid(row=6,column=3)
+
+b6 = Button(window, text="Close",width=9,command=view)
+b6.grid(row=7,column=3)
+
+
+window.mainloop()
